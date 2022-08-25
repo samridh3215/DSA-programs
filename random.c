d799930b56adbb0b1484c1d540772910491e2d62
samridh <samridhanand.2003@gmail.com>
2022-08-25 08:53:39 +0530

    Variable array list(DSA class 25/08/2022)

--- random.c
+++ random.c
@@ -2,42 +2,59 @@
 #include <stdlib.h>
 #define max 50
 
-typedef struct arrlist{
-    int a[max];
+typedef struct vararr{
+    int *a;
     int index;
-}arrlist;
+    int size;
+}VARLIST;
 
-void init(arrlist *p){
+void init(VARLIST *p){
     p->index = -1;
+    p->size = 0;
 }
 
-int append(arrlist *p, int data){
-    if(p->index == max-1)
+void array_alloc(VARLIST *p){
+    if(p->size == 0){
+        p->a = (int *)malloc(max*sizeof(int));
+        p->size = max;
+    }
+    else if(p->index ==p->size-1){
+        p->a = (int *)realloc(p->a, 2*(p->size)*sizeof(int));
+        p->size = 2*(p->size);
+    }
+}
+
+int append(VARLIST *p, int data){
+    array_alloc(p);
+    if(p->index == p->size-1)
         return 0;
     p->index++;
     p->a[p->index] = data;
     return 1;
 }
 
-int dellist(arrlist *p){
-    if(p->index==-1)
+int delete(VARLIST *p, int *pdata){
+    if(p->index == -1)
         return 0;
-    int *ptrdata;
-    *ptrdata = p->a[p->index];
+    *pdata = p->a[p->index];
     p->index--;
     return 1;
 }
 
-void display(arrlist *p){
-    for(int i =0; i<=p->index; i++){
-        printf("%d\n", p->a[i]);
+void display(VARLIST *p){
+    if(p->index == -1)
+        printf("list is empty");
+    else{
+        for(int i = 0; i<p->index;  i++){
+            printf("%d\n", p->a[i]);
+        }
     }
 }
 
 void main(){
-    arrlist mylist;
+    VARLIST mylist;
     init(&mylist);
-    int choice, res, ele;
+    int choice, res, ele, *a;
     do{
         printf("1. Append, 2. Delete Last, 3. Display\n");
         scanf("%d", &choice);
@@ -48,7 +65,7 @@ void main(){
                 append(&mylist, ele);
                 break;
 
-        case 2: dellist(&mylist);
+        case 2: delete(&mylist, a);
         break;
         
         case 3: display(&mylist);
