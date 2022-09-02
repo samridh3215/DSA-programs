2d5a0f9415ab9fbba4de5a2074766c3750595872
samridh <samridhanand.2003@gmail.com>
2022-09-02 14:36:09 +0530

    made it dynamic

--- stack.c
+++ stack.c
@@ -1,10 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #define max 10
-#include <stdbool.h>
 int count = 0;
 typedef struct Stack{
-    int topa;
+    int top;
     int items[max];
 }s;
 /*Functions of stack
@@ -16,35 +15,42 @@ Pop
 PrintStack
 Seek*/
 
-void create(s* pt){
+s* create(s* pt){
+    pt= (s*)malloc(sizeof(s));
     pt->top = -1; //top is element and pt is the pointer pointing at it
-    printf("stack created");
+    printf("stack created\n");
+    return pt;
+    
 }
 
 int isfull(s* pt){
     if (pt->top == max-1){
-        printf("full");
-        return true;
+        return 1;
         }
     else
-        return false;
+        return 0;
 }
 
 int isempty(s* pt){
     if (pt->top == -1){
         printf("empty");
-        return true;
+        return 1;
     }
     else
-        return false;
+        return 0;
                     }
 
-void push(s* pt,int a){
+void push(s* pt){
+    
+    
     if (isfull(pt))
-        printf("stack is full");
+        printf("\nstack is full");
     else{
-    pt->top++;
-    pt->items[pt->top] = a;
+        int a;
+        printf("enter number: ");
+        scanf("%d", &a);
+        pt->top++;
+        pt->items[pt->top] = a;
 
         }
         count++;
@@ -59,39 +65,51 @@ void pop(s* pt){
         printf("popped!");
     }
 
-                        }
-int seek(s* pt){
+}
+
+void peek(s* pt){
      if (isempty(pt))
-        printf("stack is empty");
+        printf("\nstack is empty");
     else{
-        printf("%d", pt->items[pt->top]);
+        printf("%d\n", pt->items[pt->top]);
 }
 }
+
 void printstack(s* st){
     for(int i = 0; i<count; i++){
             printf("%d\n", st->items[i]);
 }
 }
 int main(){
-    s* mystack = (s*)malloc(sizeof(s));
+    s mystack;
+    s* s_pointer;
+    
+    s_pointer = create(&mystack);
+    int response;
+    do{
 
-    create(mystack);
-    printf("\n");
-    isempty(mystack);
-    push(mystack, 232);
-    printf("\n");
-    push(mystack, 324);
-    printf("\n");
-    push(mystack, 23);
-    printf("\n");
-    push(mystack, 69);
-    printf("\n");
-    seek(mystack);
-    printf("\n");
-    pop(mystack);
-    printf("\n");
-    printstack(mystack);
+        printf("0 to exit\n1. To push\n2. To pop\n3. To peek\n4. Print stack");
+        scanf("%d", &response);
+        switch (response)
+        {
+        case 0:
+            break;
+        
+        case 1:
+            push(s_pointer);
+            break;
+        case 2:
+            pop(s_pointer);
+            break;
+        
+        case 3:
+            peek(s_pointer);
+            break;
+        case 4:
+            printstack(s_pointer);
+            break;
+        }
 
-
+    }while(response);
     return 0;
 }
